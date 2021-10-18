ef0c94cfb1a48a48757160f29ce8dc98bdb477d3
Abdullah Bajaman <ambajaman@gmail.com>
2021-10-18 16:07:40 +0300

    add controller

--- testGithub/ViewController.swift
+++ testGithub/ViewController.swift
@@ -8,11 +8,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var arr = ["salim","aziz"]
-    
+    @IBOutlet weak var question: UILabel!
+    @IBOutlet weak var answer: UILabel!
+    var counter = 0
+    var quiz = ["Which prophet is related to Ship and Sea? ", "Which propher is also called as Ahmed?"]
+    @IBAction func onClickAnswer(_ sender: Any) {
+        answer.text = "You should know the answer. why u ask"
+        counter += 1
+        question.text = quiz[counter]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        question.text = quiz.first
+        answer.text = "Guess!"
     }
 
 
